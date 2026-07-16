b692af5df0da7b32c47ecb3145dccfbf732c2701
Amcen <amcen@example.com>
2026-07-16 15:59:37 +0800

    Serve homepage from root

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ const bookingsDb = new sqlite3.Database(BOOKINGS_FILE, (err) => {
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const initDb = () => {
   db.serialize(() => {
